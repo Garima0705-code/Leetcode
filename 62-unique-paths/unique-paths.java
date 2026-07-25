ae25ff9b9e4515d922754010213add85a0959acf
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-07-25 13:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (66.51%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,12 +1,13 @@
 class Solution {
-    public int count(int m, int n , int row , int col , int[][] dp){
-        if(row == m-1 || col == n-1) return 1 ;
-        if(dp[row][col] != -1) return dp[row][col] ;
-        return dp[row][col] = count(m , n , row+1 , col , dp) + count(m , n , row , col+1 , dp) ;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n] ;
-        for(int i = 0 ; i < m ; i++) Arrays.fill(dp[i] , -1) ;
-        return count(m , n , 0 , 0 , dp) ;
+        Arrays.fill(dp[0] , 1) ;
+        for(int j = 1 ; j < m ; j++) dp[j][0] = 1 ;
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] ;
+            }
+        }
+        return dp[m-1][n-1] ;
     }
 }
