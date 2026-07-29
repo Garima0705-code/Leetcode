9c0baa5c72dd83863fc5f07c5484c0b62b14ae16
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-07-29 12:33:19 +0530

    Time: 1 ms (86.86%) | Memory: 46.8 MB (97.60%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,30 +1,22 @@
 class Solution {
-    public void bfs(int[][] adj , int root , boolean[] visit){
-        Queue<Integer> q = new LinkedList<>() ;
-        visit[root] = true ;
-        q.add(root) ;
-        while(!q.isEmpty()){
-            int front = q.remove() ;
-            int m = adj[front].length ;
-            for(int j = 0 ; j < m ; j++){
-                if(front != j && adj[front][j] == 1){
-                    if(visit[j] == false){
-                        q.add(j) ;
-                        visit[j] = true ;
-                    } 
-                }
+    public void dfs(int[][] adj, boolean[] visit, int i) {
+        visit[i] = true ;
+        for(int j = 0; j < adj[i].length ; j++){
+            if(i != j && adj[i][j] == 1 && !visit[j]){
+                dfs(adj,visit,j) ;
             }
         }
     }
     public int findCircleNum(int[][] adj) {
-        int n = adj.length ;
-        boolean[] visit = new boolean[n] ;
+        int m = adj.length ;
+        int n = adj[0].length ;
         int count = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            if(visit[i] == false){
-                bfs(adj ,i ,  visit) ;
+        boolean[] visit = new boolean[m] ;
+        for(int i = 0 ; i < m ; i++){
+            if(!visit[i]){
+                dfs(adj,visit,i) ;
                 count++ ;
-            }
+            }  
         }
         return count ;
     }
