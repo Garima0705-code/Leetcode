a50b801d841d119bf4cc2617c04ac532c7b740f0
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-01 12:10:46 +0530

    Time: 14 ms (32.63%), Space: 48.7 MB (76.55%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,48 +1,17 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int des ;
-        int cost ;
-        Pair(int des, int cost){
-            this.des = des ;
-            this.cost = cost ;
-        }
-        public int compareTo(Pair p){
-            return this.cost-p.cost ;
-        }
-    }
-    public int networkDelayTime(int[][] times, int n, int k) {
-        int[] cost = new int[n+1] ;
-        cost[k] = 0 ;
-        for(int i = 1 ; i <= n ; i++) if(i != k) cost[i] = Integer.MAX_VALUE ;
-        List<List<Pair>> adj = new ArrayList<>() ;
-        for(int i = 0 ; i <= n ; i++){
-            adj.add(new ArrayList<>()) ;
-        }
-        for(int i = 0 ; i < times.length ; i++){
-            int idx = times[i][0] ;
-            int des = times[i][1] ;
-            int amt = times[i][2] ;
-            adj.get(idx).add(new Pair(des,amt)) ;
-        }
-        PriorityQueue<Pair> pq = new PriorityQueue<>() ;
-        pq.add(new Pair(k,0));
-        while(pq.size() > 0){
-            Pair p = pq.remove() ;
-            int parent = p.des ;
-            int dist = p.cost ;
-            if(dist <= cost[parent]){
-                for(Pair s : adj.get(parent)){
-                    int total = dist + s.cost ;
-                    if(total < cost[s.des]){
-                        pq.add(new Pair(s.des,total)) ;
-                        cost[s.des] = total ;
-                    }
-                }
+    public int networkDelayTime(int[][] times, int n, int src) {
+        int[] dist = new int[n+1] ;
+        Arrays.fill(dist , Integer.MAX_VALUE);
+        dist[src] = 0 ;
+        for(int i = 1 ; i <= n-1 ; i++){
+            for(int j = 0 ; j < times.length ; j++){
+                if(dist[times[j][0]] == Integer.MAX_VALUE) continue ;
+                int amount = times[j][2] + dist[times[j][0]] ;
+                if(amount < dist[times[j][1]]) dist[times[j][1]] = amount ;
             }
         }
         int max = Integer.MIN_VALUE ;
-        for(int ele : cost)
-            if(ele > max) max = ele ;
+        for(int i = 1 ; i < dist.length ; i++) max = Math.max(max , dist[i]) ;
         return (max == Integer.MAX_VALUE)? -1 : max ;
     }
 }
