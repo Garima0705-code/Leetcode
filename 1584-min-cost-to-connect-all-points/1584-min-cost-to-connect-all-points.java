b1b54815830566174a1a03767f3b5b2ef21e3dd2
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-01 10:15:01 +0530

    Time: 1285 ms (5.03%), Space: 129.4 MB (19.39%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,37 @@
+class Solution {
+    class Triplet implements Comparable<Triplet>{
+        int ele ;
+        int parent ;
+        int dist ;
+        Triplet(int ele , int parent , int dist){
+            this.ele = ele ;
+            this.parent = parent ;
+            this.dist = dist ;
+        }
+        public int compareTo(Triplet t){
+            return this.dist-t.dist;
+        }
+    }
+    public int minCostConnectPoints(int[][] points) {
+        int m = points.length ;
+        boolean[] visit = new boolean[m] ;
+        int sum = 0 ;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        pq.add(new Triplet(0,-1,0));
+        while(pq.size() > 0){
+            Triplet t = pq.remove();
+            if(visit[t.ele]) continue ;
+            visit[t.ele] = true ;
+            sum += t.dist ;
+            for(int i = 0 ; i < m ; i++){
+                if(i != t.ele && i != t.parent){
+                    int x2 = points[i][0] , y2 = points[i][1] ;
+                    int x1 = points[t.ele][0] , y1 = points[t.ele][1] ;
+                    int dist = Math.abs(x1-x2) + Math.abs(y1-y2) ;
+                    pq.add(new Triplet(i , t.ele , dist));
+                }
+            }
+        }
+        return sum;
+    }
+}
