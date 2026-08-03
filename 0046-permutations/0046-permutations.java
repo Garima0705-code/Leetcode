26fccac058538df23ffef30662f2de04437aff39
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-03 12:33:34 +0530

    Time: 1 ms (87.14%), Space: 44.8 MB (99.31%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,19 +1,25 @@
 class Solution {
-    public void backtrack(int[] nums , List<List<Integer>> result , List<Integer> perm){
-        if(perm.size() == nums.length){
-            result.add(new ArrayList<>(perm)) ;
+    public void permutation(int[] nums, List<List<Integer>> ans, List<Integer> list, boolean[] visit){
+
+        if(list.size() == nums.length){
+            ans.add(new ArrayList<>(list));
             return ;
         }
-        for(int i = 0 ; i < nums.length ; i++){
-            if(perm.contains(nums[i])) continue ;
-            perm.add(nums[i]) ;
-            backtrack(nums , result , perm) ;
-            perm.remove(perm.size()-1) ;
+        for(int i = 0; i < nums.length; i++){
+            if(visit[i]) continue ;
+            int ele = nums[i];
+            list.add(ele);
+            visit[i] = true ;
+            permutation(nums, ans, list, visit);
+            visit[i] = false;
+            list.remove(list.size()-1);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>() ;
-        backtrack(nums , result , new ArrayList<>()) ;
-        return result ;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean[] visit = new boolean[nums.length];
+        permutation(nums, ans, list, visit);
+        return ans ;
     }
 }
