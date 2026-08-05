64f394fb2299067dd9a63aecf25b061deb8ad963
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-05 19:34:55 +0530

    Time: 23 ms (40.49%) | Memory: 54.3 MB (6.61%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,33 @@
 class Solution {
-    public int lcs(String s1, String s2, int si, int sj, int[][] dp) {
-        if(si < 0 || sj < 0) return 0 ;
-        if(dp[si][sj] != -1) return dp[si][sj] ;
-        if(s1.charAt(si) == s2.charAt(sj)){
-            return dp[si][sj] = 1 + lcs(s1 , s2 , si-1 , sj-1, dp) ;
-        }else{
-            return dp[si][sj] = Math.max(lcs(s1 , s2 , si-1 , sj, dp) , lcs(s1 , s2 , si , sj-1, dp)) ;
-        }
-    }
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length() ;
         int n = text2.length() ;
-        int[][] dp = new int[m][n] ;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(text1.charAt(i) == text2.charAt(j)){
-                    int z = (i <= 0 || j <= 0) ? 0 : dp[i-1][j-1] ;
-                    dp[i][j] = 1 + z ;
+        int[][] dp = new int[m+1][n+1] ;
+        for(int i = 1 ; i <= m ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1] ;
                 }else{
-                    int x = (i <= 0) ? 0 : dp[i-1][j] ;
-                    int y = (j <= 0) ? 0 : dp[i][j-1] ;
-                    dp[i][j] = Math.max(x , y) ;
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]) ;
                 }
             }
         }
-        return dp[m-1][n-1];
+        StringBuilder sb = new StringBuilder();
+        int i = m , j = n;
+        while(i > 0 && j > 0){
+            if(text1.charAt(i-1) == text2.charAt(j-1)){
+                sb.append(text1.charAt(i-1));
+                i--;
+                j--;
+            }else{
+                if(dp[i-1][j] > dp[i][j-1]) i--;
+                else j--;
+            }
+        }
+        sb.reverse();
+        System.out.print(sb);
+        return dp[m][n];
     }
 }
+
+       
