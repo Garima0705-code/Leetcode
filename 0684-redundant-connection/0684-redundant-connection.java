7d09c15be967f8d0422dfdbf448892249ba962f9
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-08 18:54:00 +0530

    Time: 1 ms (91.74%), Space: 45.1 MB (38.23%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,42 @@
+class Solution {
+    int[] parent ;
+    int[] size;
+    public int leader(int u){
+        if(parent[u] == u) return u ;
+        return parent[u] = leader(parent[u]);
+    }
+    public void union(int u , int v){
+        int a = leader(u) ;
+        int b = leader(v) ;
+        if(a != b){
+            if(size[a] > size[b]){
+                parent[b] = a;
+                size[a] += size[b];
+            }else{
+                parent[a] = b;
+                size[b] += size[a];
+            }
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length ;
+        parent = new int[n+1] ;
+        size = new int[n+1] ;
+        for(int i = 0 ; i <= n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+        int[] ans = new int[2] ;
+        for(int[] ele : edges){
+            int u = ele[0] , v = ele[1] ;
+            if(leader(u) == leader(v)){
+                ans[0] = u ;
+                ans[1] = v ;
+                break ;
+            }else{
+                union(u , v);
+            }
+        }
+        return ans ;
+    }
+}
