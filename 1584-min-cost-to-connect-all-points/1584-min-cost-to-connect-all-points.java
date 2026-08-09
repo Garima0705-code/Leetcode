5dab401bcd13e2bf9e9a16e004b1101004a90fdf
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-09 10:45:20 +0530

    Time: 1258 ms (5.01%), Space: 128.9 MB (20.35%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,38 +1,62 @@
 class Solution {
     class Triplet implements Comparable<Triplet>{
-        int ele ;
-        int parent ;
+        int x ;
+        int y ;
         int dist ;
-        Triplet(int ele , int parent , int dist){
-            this.ele = ele ;
-            this.parent = parent ;
+        Triplet(int x , int y , int dist){
+            this.x = x ;
+            this.y = y ;
             this.dist = dist ;
         }
         public int compareTo(Triplet t){
             return this.dist-t.dist;
         }
     }
+    public int leader(int u, int[] parent){
+        if(parent[u] == u) return u;
+        return parent[u] = leader(parent[u], parent) ;
+    }
+    public void union(int u, int v, int[] parent, int[] size){
+        int a = leader(u, parent) ;
+        int b = leader(v, parent) ;
+        if(size[a] > size[b]){
+            parent[b] = a;
+            size[a] += size[b] ;
+        }else{
+            parent[a] = b;
+            size[b] += size[a] ;
+        }
+    }
     public int minCostConnectPoints(int[][] points) {
-        int m = points.length ;
-        boolean[] visit = new boolean[m] ;
-        int sum = 0 ;
-        PriorityQueue<Triplet> pq = new PriorityQueue<>();
-        pq.add(new Triplet(0,-1,0));
-        while(pq.size() > 0){
-            Triplet t = pq.remove();
-            if(visit[t.ele]) continue ;
-            visit[t.ele] = true ;
-            sum += t.dist ;
-            for(int i = 0 ; i < m ; i++){
-                if(visit[i] == true) continue ;
-                if(i != t.ele && i != t.parent){
-                    int x2 = points[i][0] , y2 = points[i][1] ;
-                    int x1 = points[t.ele][0] , y1 = points[t.ele][1] ;
-                    int dist = Math.abs(x1-x2) + Math.abs(y1-y2) ;
-                    pq.add(new Triplet(i , t.ele , dist));
-                }
+        int n = points.length ;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>() ;
+        for(int i = 0; i < n ; i++){
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            for(int j = 0 ; j < n ; j++){
+                if(i == j) continue ;
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int dist = Math.abs(x1-x2) + Math.abs(y1-y2) ;
+                pq.add(new Triplet(i,j,dist)) ;
             }
         }
-        return sum;
+        int[] parent = new int[n] ;
+        int[] size = new int[n] ;
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i ;
+            size[i] = 1;
+        }
+        int min = 0;
+        while(pq.size() > 0){
+            Triplet t = pq.remove() ;
+            int u = t.x ;
+            int v = t.y ;
+            if(leader(u, parent) != leader(v, parent)){
+                min += t.dist ;
+                union(u,v, parent,size);
+            }
+        }
+        return min;
     }
 }
