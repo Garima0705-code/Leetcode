548e7d398e3df934138581341fed198f09fb79da
Garima0705-code <garimabhardwaj0705@gmail.com>
2026-08-18 16:36:10 +0530

    Time: 1 ms (99.95%), Space: 94.4 MB (71.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -5,14 +5,12 @@ class Solution {
         int max = arr[n-1] ;
         for(int i = n-2 ; i >= 0 ; i--){
             int ele = arr[i] ;
-            arr[i] = 0;
             if(ele < max){
                 int diff = max-ele ;
-                arr[i] = Math.max(diff,arr[i]) ;
+                maxPro = Math.max(maxPro,diff) ;
             }else{
                 max = ele ;
             }
-            maxPro = Math.max(maxPro,arr[i]) ;
         }
         return maxPro ;
     }
